2d6c7c1748942500b135e75caffed903878421cf
BuildTools <abdullahzafarrana@gmail.com>
2023-06-09 00:17:47 +0500

    nothing done

---
+++ src/main/java/org/refabricators/totemexpansion/TotemExpansionClient.java
@@ -0,0 +1,10 @@
+package org.refabricators.totemexpansion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TotemExpansionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
