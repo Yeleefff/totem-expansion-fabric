687a347ebcdb9f7b39d13b3bcb7a13f67256c14d
TechPro424 <smsvra6@gmail.com>
2023-06-11 16:20:22 +0530

    added the addDamageSources() method

--- src/main/java/org/refabricators/totemexpansion/item/BaseTotem.java
+++ src/main/java/org/refabricators/totemexpansion/item/BaseTotem.java
@@ -1,5 +1,7 @@
 package org.refabricators.totemexpansion.item;
 
+import java.util.ArrayList;
+
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
@@ -8,10 +10,14 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Rarity;
 
 public abstract class BaseTotem extends Item {
+    public ArrayList<DamageSource> damageSources = new ArrayList<DamageSource>();
 
     public BaseTotem() {
         super(new FabricItemSettings().maxCount(1).rarity(Rarity.UNCOMMON));
+        addDamageSources();
     }
+
+    public abstract void addDamageSources();
 
     public abstract void onTotemUse(LivingEntity entity, DamageSource source, ItemStack stack);
     
